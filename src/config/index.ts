673300f2cd99f3295853e5092d0eb1636232b672
deep-madhvani <deep.madhvani@csu.fullerton.edu>
2023-11-10 00:19:19 -0800

    Test 2

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,9 +3,9 @@
 export default {
   nodeEnv: (process.env.NODE_ENV || 'development'),
   port: (process.env.PORT || 3001),
-  brightData_bearerToken: ('5bc93e7a-a31d-4a6b-893a-e269d9e85084'),
-  brightData_collectorId: ('c_lorxaaxp1kv74f4nd4'),
-  mongo_connectionString: ('mongodb+srv://deepmadhvani0543:Deep123@cluster0.dgdfn5n.mongodb.net/?retryWrites=true&w=majority'),
+  brightData_bearerToken: (process.env.API_KEY || ''),
+  brightData_collectorId: (process.env.COLLECTOR_ID || ''),
+  mongo_connectionString: ("mongodb+srv://deepmadhvani0543:Deep123@cluster0.dgdfn5n.mongodb.net/?retryWrites=true&w=majority" || ''),
 } as const;
 
 	
